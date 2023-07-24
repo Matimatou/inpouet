fb96f75115ec9b741c95425db82d18e7ce81c74d
Mathias <Inutile0@protonmail.com>
2023-07-24 16:00:36 +0200

    improvement: Slideshow - is now made by dom manipulation - can put picture with a simple array

--- script.js
+++ script.js
@@ -18,6 +18,42 @@ function closeMenu() {
 }
 
 // Diaporama 
+const imgPath = ["gala_1.jpg", "gala_2.jpg", "gala_3.jpg", "léonor.jpg"]
+function fillSlideContainer() {
+    const slideContainer = document.querySelector('.slideshow-container');
+    const dotContainer = document.querySelector('.dot-container');
+    function createSlide(index) {
+        // Create the slide with proper class
+        const slide = document.createElement('div');
+        slide.classList.add('mySlides', 'fade');
+        // Create the number text
+        const numberText = document.createElement('div');
+        numberText.classList.add('numbertext');
+        numberText.innerText = `${index + 1} / ${imgPath.length}`;
+        // Create the image
+        const img = document.createElement('img');
+        img.src = `img/${imgPath[index]}`;
+        //img.style.width = "100%";
+        // Add the elements to the slide
+        slide.appendChild(numberText);
+        slide.appendChild(img);
+        // Add the slide to the container
+        slideContainer.appendChild(slide);
+        console.log(slide, slideContainer);
+    }
+    function createDot(index) {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dot.addEventListener('click', () => currentSlide(index));
+        dotContainer.appendChild(dot);
+    }
+    for (let i = 0; i < imgPath.length; i++) {
+        createSlide(i);
+        createDot(i);
+    }
+}
+
+fillSlideContainer();
 
 let slideIndex = 0;
 let timeoutId = null;
@@ -25,20 +61,19 @@ const slides = document.getElementsByClassName("mySlides");
 const dots = document.getElementsByClassName("dot");
 
 showSlides();
+
 function currentSlide(index) {
     slideIndex = index;
     showSlides();
 }
 function plusSlides(step) {
+    slideIndex += step;
 
-    if (step < 0) {
-        slideIndex -= 2;
-
-        if (slideIndex < 0) {
-            slideIndex = slides.length - 1;
-        }
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    } else if (slideIndex >= slides.length) {
+        slideIndex = 0;
     }
-
     showSlides();
 }
 function showSlides() {
@@ -46,14 +81,20 @@ function showSlides() {
         slides[i].style.display = "none";
         dots[i].classList.remove('active');
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].classList.add('active');
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].classList.add('active');
+
     if (timeoutId) {
         clearTimeout(timeoutId);
     }
-    timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
+    timeoutId = setTimeout(() => {
+        plusSlides(1);
+    }, 500000); // Change image every 5 seconds
 }
